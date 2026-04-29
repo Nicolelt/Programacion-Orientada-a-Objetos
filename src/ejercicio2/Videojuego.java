8bc918aa17e89493e92f91690f29a20900b65974
Nicole9206 <nic.latfig@gmail.com>
2026-04-29 12:04:35 -0400

    Ejercicio poo2

---
+++ src/ejercicio2/Videojuego.java
@@ -0,0 +1,43 @@
+package ejercicio2;
+
+public class Videojuego{
+    private final String nombre;
+    private final int epoca;
+    private final double precio;
+    private final int duracion;
+    private final Genero genero;
+    private final Empresa empresa;
+
+    
+
+    public Videojuego(String nombre, int epoca, double precio, int duracion, Genero genero, Empresa empresa) {
+        this.nombre = nombre;
+        this.epoca = epoca;
+        this.precio = precio;
+        this.duracion = duracion;
+        this.genero = genero;
+        this.empresa = empresa;
+    }
+
+    
+    public enum Genero {
+        TERROR,
+        ACCION, 
+        COMEDIA
+    }
+    public enum Empresa {
+        XBOX, 
+        NINTENDO, 
+        PLAY_STATION
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public Genero getGenero() {
+        return genero;
+    }
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+}
